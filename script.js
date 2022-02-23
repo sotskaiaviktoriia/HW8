d14abfc7f9599804aeb3f313ced0d96ba0ba72c7
Victoria <victoriyasotscaya2050@gmail.com>
2022-02-24 01:07:35 +0200

    Added Task2

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ function sumTwoSmallestNumbers(...array) {
 console.log(sumTwoSmallestNumbers(19, 5, 42, 2, 77));
 console.log(sumTwoSmallestNumbers(1, 3, 2));
 console.log(sumTwoSmallestNumbers(1));
+
+//Task2
+function createCalculator(arg) {
+  let count = arg;
+
+  const objectOperation = {
+    sum: (num) => (count += num),
+    mult: (num) => (count *= num),
+    sub: (num) => (count -= num),
+    div: (num) => (count /= num),
+  };
+  return objectOperation;
+}
+
+const calc = createCalculator(10);
+
+console.log(calc.sum(5));
+console.log(calc.mult(10));
+console.log(calc.sub(40));
+console.log(calc.div(10));
